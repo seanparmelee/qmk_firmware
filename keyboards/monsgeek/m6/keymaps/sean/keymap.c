e7aa3ef80a5e6e6f16b50dbf4d74b56e55969ee0
Sean Parmelee <sean.parmelee@x.studio>
2023-11-07 08:52:49 -0600

    swap ESC with KC_GRV magic

--- keyboards/monsgeek/m6/keymaps/sean/keymap.c
+++ keyboards/monsgeek/m6/keymaps/sean/keymap.c
@@ -53,11 +53,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,                   _______, _______,          GU_TOGG, _______,                  _______, _______,           RGB_SAD,  RGB_VAD, RGB_SAI),
 
     [MAC_B] = LAYOUT( /* Base */
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,KC_EQL,   KC_BSPC,          KC_HOME,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,KC_RBRC,  KC_BSLS,          KC_PGUP,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,KC_NUHS,  KC_ENT,           KC_PGDN,
-        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT, KC_UP,
-        KC_LCTL,                   KC_LGUI, KC_SPC,           KC_LALT, KC_SPC,                    KC_RGUI, MO(WIN_FN),       KC_LEFT, KC_DOWN, KC_RGHT),
+        LT(0,KC_GRV),   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,KC_EQL,   KC_BSPC,          KC_HOME,
+        KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,KC_RBRC,  KC_BSLS,          KC_PGUP,
+        KC_CAPS,        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,KC_NUHS,  KC_ENT,           KC_PGDN,
+        KC_LSFT,        KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT, KC_UP,
+        KC_LCTL,                          KC_LGUI, KC_SPC,           KC_LALT, KC_SPC,                    KC_RGUI, MO(WIN_FN),       KC_LEFT, KC_DOWN, KC_RGHT),
 
     [MAC_W] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
@@ -67,10 +67,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                   _______,                            _______, _______, _______,          KC_A,    KC_S,    KC_D),
 
     [MAC_FN] = LAYOUT( /* FN */
-        KC_GRV,  KC_BRID, KC_BRIU, KC_MCTL, KC_LPAD, KC_F5,   KC_F6,   KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,KC_VOLU,  _______,           RGB_MOD,
+        KC_ESC,  KC_BRID, KC_BRIU, KC_MCTL, KC_LPAD, KC_F5,   KC_F6,   KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,KC_VOLU,  _______,           RGB_MOD,
         _______, _______,TG(MAC_W),_______, _______, _______, _______, _______, KC_INS,  DF(WIN_B),KC_PSCR,_______, _______, _______,           KC_HOME,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, RGB_SPD, RGB_SPI, _______, RGB_HUI,           KC_END,
         _______, _______, _______, _______, KC_CALC, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,  RGB_VAI,
         _______,                   _______, _______,          _______, _______,                  _______, _______,           RGB_SAD,  RGB_VAD, RGB_SAI),
 };
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LT(0,KC_GRV):
+            if (record->tap.count && record->event.pressed) {
+                return true; // Return true for normal processing of tap keycode
+            } else if (record->event.pressed) {
+                tap_code(KC_ESC); // Intercept hold function to send escape    
+                return false;
+            }
+        default:
+            return true; // Process all other keycodes normally
+    }
+}
