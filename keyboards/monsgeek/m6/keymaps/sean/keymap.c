164ec7b09629aa34549d947eb5c6052ca6cb4ace
Sean Parmelee <sean.parmelee@thinkonward.com>
2024-11-19 09:55:54 -0600

    add volume shortcuts

--- keyboards/monsgeek/m6/keymaps/sean/keymap.c
+++ keyboards/monsgeek/m6/keymaps/sean/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,KC_RBRC,  KC_BSLS,          KC_PGUP,
         KC_CAPS,        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,KC_NUHS,  KC_ENT,           KC_PGDN,
         KC_LSFT,        KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT, KC_UP,
-        KC_LCTL,                          KC_LGUI, KC_SPC,           KC_LALT, KC_SPC,                    KC_RGUI, MO(WIN_FN),       KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL,                          KC_LGUI, KC_SPC,           KC_LALT, KC_SPC,                    KC_RGUI, MO(MAC_FN),       KC_LEFT, KC_DOWN, KC_RGHT),
 
     [MAC_W] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
@@ -68,8 +68,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [MAC_FN] = LAYOUT( /* FN */
         KC_ESC,  KC_BRID, KC_BRIU, KC_MCTL, KC_LPAD, KC_F5,   KC_F6,   KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,KC_VOLU,  _______,           RGB_MOD,
-        _______, _______,TG(MAC_W),_______, _______, _______, _______, _______, KC_INS,  DF(WIN_B),KC_PSCR,_______, _______, _______,           KC_HOME,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, RGB_SPD, RGB_SPI, _______, RGB_HUI,           KC_END,
+        _______, _______,TG(MAC_W),_______, _______, _______, _______, _______, KC_INS,  DF(WIN_B),KC_PSCR,_______, _______, _______,           KC_VOLU,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, RGB_SPD, RGB_SPI, _______, RGB_HUI,           KC_VOLD,
         _______, _______, _______, _______, KC_CALC, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,  RGB_VAI,
         _______,                   _______, _______,          _______, _______,                  _______, _______,           RGB_SAD,  RGB_VAD, RGB_SAI),
 };
